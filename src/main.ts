19f3f88ab2ac12a48721ed5bc8c199299ddde5ce
Blessings Chidambe <bchidambe@gmail.com>
2021-11-03 20:21:02 +0200

    Adding taiwind css

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,7 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 import './theme/global.css';
+import './theme/tailwind.css';
 
 const app = createApp(App)
   .use(IonicVue)
