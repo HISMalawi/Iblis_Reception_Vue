b18e37f07a62cde673d39e4486a6ae3ec009b890
Blessings Chidambe <bchidambe@gmail.com>
2021-11-03 21:24:19 +0200

    Creating Add Patient & Create Order Pages

---
+++ src/views/AddPatient.vue
@@ -0,0 +1,17 @@
+<template>
+    <BaseLayout pageTitle="Add Patient">
+    </BaseLayout>
+    
+</template>
+
+<script lang="ts">
+import BaseLayout from '../components/base/BaseLayout.vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'AddPatient',
+  components: {
+    BaseLayout,
+  }
+});
+</script>
---
+++ src/views/CreateOrder.vue
@@ -0,0 +1,17 @@
+<template>
+    <BaseLayout pageTitle="Create Order">
+    </BaseLayout>
+    
+</template>
+
+<script lang="ts">
+import BaseLayout from '../components/base/BaseLayout.vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CreateOrder',
+  components: {
+    BaseLayout,
+  }
+});
+</script>
