74c7e5077c87c5f7783e682f56d6832818017ce4
Blessings Chidambe <bchidambe@gmail.com>
2021-11-04 14:08:50 +0200

    UI update and setting routes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,25 +5,21 @@
     <!-- We've used 3xl here, but feel free to try other max-widths based on your needs -->
     <div class="max-w-3xl mx-auto">
       <!-- Content goes here -->
-          <button type="button" class="bg-white shadow mb-10 relative block w-full border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-            <!-- <svg class="mx-auto h-12 w-12 text-gray-400" xmlns="http://www.w3.org/2000/svg" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 14v20c0 4.418 7.163 8 16 8 1.381 0 2.721-.087 4-.252M8 14c0 4.418 7.163 8 16 8s16-3.582 16-8M8 14c0-4.418 7.163-8 16-8s16 3.582 16 8m0 0v14m0-4c0 4.418-7.163 8-16 8S8 28.418 8 24m32 10v6m0 0v6m0-6h6m-6 0h-6" />
-            </svg> -->
+          <ion-nav-link router-link="/add_patient" class="bg-white shadow mb-10 relative block w-full border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
             <ion-icon :icon="personAdd"></ion-icon>
             <span class="mt-2 block text-sm font-medium text-gray-900">
               Add Patient
             </span>
-          </button>
+          </ion-nav-link>
 
 
-          <button type="button" class="bg-white shadow elative block w-full border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-            <svg class="mx-auto h-12 w-12 text-gray-400" xmlns="http://www.w3.org/2000/svg" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 14v20c0 4.418 7.163 8 16 8 1.381 0 2.721-.087 4-.252M8 14c0 4.418 7.163 8 16 8s16-3.582 16-8M8 14c0-4.418 7.163-8 16-8s16 3.582 16 8m0 0v14m0-4c0 4.418-7.163 8-16 8S8 28.418 8 24m32 10v6m0 0v6m0-6h6m-6 0h-6" />
-            </svg>
+          <ion-nav-link router-link="/create_order" class="bg-white shadow elative block w-full border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <ion-icon :icon="fileTrayFull"></ion-icon>
             <span class="mt-2 block text-sm font-medium text-gray-900">
               Create Order
             </span>
-          </button>
+          </ion-nav-link>
+
     </div>
   </div>
 
@@ -38,18 +34,19 @@
 <script lang="ts">
 import BaseLayout from '../components/base/BaseLayout.vue';
 import { defineComponent } from 'vue';
-import { IonIcon } from "@ionic/vue";
-import { star , personAdd } from "ionicons/icons";
+import { IonIcon, IonNavLink} from "@ionic/vue";
+import { personAdd , fileTrayFull } from "ionicons/icons";
 export default defineComponent({
   name: 'Home',
   components: {
     BaseLayout,
     IonIcon,
+    IonNavLink,
   },
   setup(){
 
 
-    return { star , personAdd}
+    return { personAdd, fileTrayFull }
   }
 });
 </script>
@@ -57,7 +54,7 @@ export default defineComponent({
 <style>
 
 .home ion-icon {
-  
+
   font-size: 64px;
 }
 
