fed21b81a271496af9c8359fecf0895258644ae0
Blessings Chidambe <45852972+Cardkess@users.noreply.github.com>
2021-12-02 17:15:34 +0200

    COde refactor & disable search as u type

--- src/components/SearchPanel.vue
+++ src/components/SearchPanel.vue
@@ -78,23 +78,23 @@ export default defineComponent({
       store.commit(MutationTypes.SET_SELECTED_PATIENT, defaultPatient);
     };
 
-    watch(
-      () => [searchString.value != searchString.value],
-      () => {
-        setTimeout(() => {
-          if (searchString.value == "") {
-            OpenPatientDetails(false);
-            ClosePatientSearchResultsView();
-          } else {
-            search(searchString.value);
-            OpenPatientDetails(false);
-            OpenPatientSearchResultsView();
-          }
+    // watch(
+    //   () => [searchString.value],
+    //   () => {
+    //     setTimeout(() => {
+    //       if (searchString.value == "") {
+    //         OpenPatientDetails(false);
+    //         ClosePatientSearchResultsView();
+    //       } else {
+    //         search(searchString.value);
+    //         OpenPatientDetails(false);
+    //         OpenPatientSearchResultsView();
+    //       }
 
-          store.commit(MutationTypes.SET_SELECTED_PATIENT, defaultPatient);
-        }, 500);
-      }
-    );
+    //       store.commit(MutationTypes.SET_SELECTED_PATIENT, defaultPatient);
+    //     }, 500);
+    //   }
+    // );
 
     return { Search, searchString };
   },
@@ -102,7 +102,7 @@ export default defineComponent({
 </script>
 <style>
 .search-card {
-  max-height: 130px;
+  max-height: 98px !important;
   max-width: 530px !important;
   margin: 0 auto;
 }
