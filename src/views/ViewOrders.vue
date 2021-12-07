b9df614b9e175b8da495b362b2b482b062907214
Blessings Chidambe <45852972+Cardkess@users.noreply.github.com>
2021-12-07 10:34:35 +0200

    Adding Tests to Order view results

--- src/views/ViewOrders.vue
+++ src/views/ViewOrders.vue
@@ -23,17 +23,59 @@
           <nav v-for="order in orders" :key="order.id">
             <p class="panel-heading">{{ order.tracking_number }}</p>
             <p class="panel-tabs">
-              <a class="is-active">Overview</a>
-              <a>Results</a>
+              <a
+                @click="showOverview"
+                :class="viewingOverview ? 'is-active' : ''"
+                >Overview</a
+              >
+              <a @click="showResults" :class="viewingResults ? 'is-active' : ''"
+                >Results</a
+              >
             </p>
-            <a class="panel-block"><h5 class="title is-6">Patient : </h5>{{order.patient[0].name}}</a>
-            <a class="panel-block"><h5 class="title is-6">Visit Type : </h5> {{order.visit_type}}</a>
-            <a class="panel-block"><h5 class="title is-6">Requesting Ward / Location :  </h5> {{order.location}}</a>
-            <a class="panel-block"><h5 class="title is-6"> Requesting Physician : </h5> {{order.requesting_physician}}</a>
-            <a class="panel-block"><h5 class="title is-6">Specimen Type : </h5>  {{order.specimen_type}}</a>
-            <a class="panel-block"><h5 class="title is-6">Tests : </h5> <button v-for="test in order.tests" :key="test.id" class="button is-rounded is-small is-warning">{{test.test_name}}</button></a>
+
+            <div v-if="viewingOverview">
+              <a class="panel-block"
+                ><h5 class="title is-6">Patient :</h5>
+                {{ order.patient[0].name }}</a
+              >
+              <a class="panel-block"
+                ><h5 class="title is-6">Visit Type :</h5>
+                {{ order.visit_type }}</a
+              >
+              <a class="panel-block"
+                ><h5 class="title is-6">Requesting Ward / Location :</h5>
+                {{ order.location }}</a
+              >
+              <a class="panel-block"
+                ><h5 class="title is-6">Requesting Physician :</h5>
+                {{ order.requesting_physician }}</a
+              >
+              <a class="panel-block"
+                ><h5 class="title is-6">Specimen Type :</h5>
+                {{ order.specimen_type }}</a
+              >
+              <a class="panel-block"
+                ><h5 class="title is-6">Tests :</h5>
+                <button
+                  v-for="test in order.tests"
+                  :key="test.id"
+                  class="button is-rounded is-small is-warning"
+                >
+                  {{ test.test_name }}
+                </button></a
+              >
+            </div>
+
+            <div v-if="viewingResults">
+              <a v-for="test in order.tests" :key="test.id" class="panel-block"
+                ><h5 class="title is-6">{{ test.test_name }} :</h5>
+                </a
+              >
+            </div>
+
+
+
           </nav>
-         
         </div>
       </div>
     </article>
@@ -41,29 +83,52 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { defineComponent, ref, watch } from "vue";
 import SearchOrder from "@/composables/searchOrder";
 
 export default defineComponent({
   name: "ViewOrders",
   components: {},
   setup() {
+    const viewingOverview = ref<boolean>(true);
+
+    const viewingResults = ref<boolean>(false);
+
     const searchString = ref<string>("");
 
     const { search, orders, message } = SearchOrder();
+
+    const showOverview = () => {
+      viewingResults.value = false;
+
+      viewingOverview.value = true;
+    };
+
+    const showResults = () => {
+      viewingOverview.value = false;
+
+      viewingResults.value = true;
+    };
+
 
     const Search = () => {
       orders.value.length = 0;
 
       if (searchString.value == "") {
       } else {
-        
         search(searchString.value);
       }
-      console.log(orders)
     };
 
-    return { orders, Search, searchString };
+    return {
+      orders,
+      Search,
+      searchString,
+      viewingOverview,
+      viewingResults,
+      showOverview,
+      showResults,
+    };
   },
 });
 </script>
