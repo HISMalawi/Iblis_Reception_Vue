f140af5e95ac0bd9d62ce678dd9a7f9310ac961b
Blessings Chidambe <45852972+Cardkess@users.noreply.github.com>
2021-12-28 08:49:40 +0200

    Adding select location or ward to login form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -11,23 +11,21 @@
           </div>
           <form @submit.prevent="Signin">
             <div class="field">
-              <p class="control has-icons-left has-icons-right">
+              <p class="control has-icons-right">
                 <input
                   class="input is-medium"
                   type="text"
                   placeholder="Username"
                   v-model="authCred.username"
                 />
-                <span class="icon is-small is-left">
-                  <i class="fas fa-envelope"></i>
-                </span>
+
                 <span class="icon is-small is-right">
                   <i class="fas fa-check"></i>
                 </span>
               </p>
             </div>
             <div class="field">
-              <p class="control has-icons-left">
+              <p class="control">
                 <input
                   class="input is-medium"
                   type="password"
@@ -35,6 +33,21 @@
                   v-model="authCred.password"
                 />
               </p>
+            </div>
+
+            <div class="field mb-6">
+              <div class="control">
+                <div class="select is-medium is-fullwidth">
+                  <select v-model="selectedWard">
+                    <option :value="selectedWard">
+                      {{ selectedWard.name }}
+                    </option>
+                    <option :value="ward" v-for="ward in Wards" :key="ward.id">
+                      {{ ward.name }}
+                    </option>
+                  </select>
+                </div>
+              </div>
             </div>
 
             <div class="field">
@@ -55,11 +68,13 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, watchEffect } from "vue";
+import { defineComponent, reactive, ref, watch, watchEffect } from "vue";
 import { AuthRequest } from "@/interfaces/AuthRequest";
 import { useRouter } from "vue-router";
 import { useStore } from "@/store";
 import Authenticate from "@/composables/authenticate";
+import GetAllWards from "@/composables/getAllWards";
+import { Ward } from "@/interfaces/Ward";
 
 export default defineComponent({
   name: "Login",
@@ -67,6 +82,15 @@ export default defineComponent({
   setup() {
     const store = useStore();
     const router = useRouter();
+
+    const selectedWard = ref<Ward>({
+      id: 0,
+      name: "--- Select Ward / Location ---",
+    });
+
+    const { Wards, fetchWards } = GetAllWards();
+
+    fetchWards();
 
     const { login, message } = Authenticate();
 
@@ -76,7 +100,11 @@ export default defineComponent({
     });
 
     const Signin = () => {
-      login(authCred);
+      if (selectedWard.value.id == 0) {
+        message.value = "Requesting Ward / Location is required"
+      } else {
+        login(authCred);
+      }
     };
 
     watchEffect(() => {
@@ -85,7 +113,14 @@ export default defineComponent({
       }
     });
 
-    return { authCred, Signin, message };
+    watch(
+      () => [selectedWard.value],
+      () => {
+        message.value = ""
+      }
+    );
+
+    return { authCred, Signin, message, Wards, selectedWard };
   },
 });
 </script>
