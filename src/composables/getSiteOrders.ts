698a7d45a84ab629bea774e192d41df761dfdb7a
Blessings Chidambe <45852972+Cardkess@users.noreply.github.com>
2022-01-11 08:00:41 +0200

    Dashboad auto data reload

--- src/composables/getSiteOrders.ts
+++ src/composables/getSiteOrders.ts
@@ -1,6 +1,7 @@
-import { ref } from "vue";
+import { ref, watch } from "vue";
 import { useStore } from "@/store";
 import { Specimen } from "@/interfaces/Specimen";
+import { TestResult } from "@/interfaces/TestResult";
 import TokenCheck from "@/composables/tokenCheck";
 
 const { logout } = TokenCheck()
@@ -8,6 +9,12 @@ const { logout } = TokenCheck()
 const store = useStore();
 
 const Specimens = ref<Specimen[]>([]);
+const TestWithResults = ref<TestResult[]>([]);
+const SpecimensWithResults = ref<number[]>([]);
+
+const BGSpecimens = ref<Specimen[]>([]);
+const BGTestWithResults = ref<TestResult[]>([]);
+const BGSpecimensWithResults = ref<number[]>([]);
 
 const getSiteOrders = () => {
 
@@ -19,6 +26,8 @@ const getSiteOrders = () => {
 
   const message = ref<string>("");
   const code = ref<string>("");
+
+
 
   const fetchOrders = () => {
 
@@ -39,7 +48,30 @@ const getSiteOrders = () => {
 
           if (code.value == "200") {
 
-            Specimens.value = responseData
+            TestWithResults.value = responseData.tests_with_results
+
+            TestWithResults.value.forEach(test => {
+
+              SpecimensWithResults.value.push(test.specimen_id)
+              
+            });
+
+            Specimens.value = responseData.specimens
+
+            for (let index = 0; index < Specimens.value.length; index++) {
+
+              if (SpecimensWithResults.value.includes(Specimens.value[index].id)) {
+
+                  Specimens.value[index] = Object.assign(Specimens.value[index], { class:"results-ready"})
+              
+              } else {
+
+                Specimens.value[index] = Object.assign(Specimens.value[index], { class:"from-db"})
+                
+              }
+              
+              
+            }
 
             message.value = response.data.message;
 
@@ -55,8 +87,85 @@ const getSiteOrders = () => {
         message.value = error.message;
       });
   };
+
+  const BGfetchOrders = () => {
+
+    axios.value
+      .post("/ward/orders", {
+        token: token.value,
+        ward:ward.value,
+      })
+      .then(function (response: any) {
+        if (response.statusText === "OK") {
+
+          if (response.data == "Invalid Token") logout()
+
+          code.value = response.data.code;
+
+          const responseData = response.data.data;
+
+
+          if (code.value == "200") {
+
+            BGTestWithResults.value = responseData.tests_with_results
+
+            BGTestWithResults.value.forEach(test => {
+
+              BGSpecimensWithResults.value.push(test.specimen_id)
+              
+            });
+
+            BGSpecimens.value = responseData.specimens
+
+            for (let index = 0; index < BGSpecimens.value.length; index++) {
+
+              if (BGSpecimensWithResults.value.includes(BGSpecimens.value[index].id)) {
+
+                  BGSpecimens.value[index] = Object.assign(BGSpecimens.value[index], { class:"results-ready"})
+              
+              } else {
+
+                BGSpecimens.value[index] = Object.assign(BGSpecimens.value[index], { class:"from-db"})
+                
+              }
+              
+              
+            }
+
+            message.value = response.data.message;
+
+          } else {
+
+            message.value = response.data.message;
+          }
+
+        }
+      })
+      .catch(function (error: any) {
+        message.value = error.message;
+      });
+  };
  
-  return { fetchOrders, message, Specimens, code };
+
+  setInterval(() => {
+
+    BGfetchOrders();
+
+  }, 10000);
+
+  watch(
+
+      () => [BGSpecimens.value],
+      () => {
+
+        
+          Specimens.value = BGSpecimens.value
+
+
+      }
+  );
+ 
+  return { fetchOrders, Specimens };
 };
 
 export default getSiteOrders;
--- src/views/OrderResult.vue
+++ src/views/OrderResult.vue
@@ -1,76 +1,82 @@
 <template>
   <div class="order-result">
-    
-      <!-- <div class="card result-card" v-for="order in $store.state.createdOrdersTrackingNum" :key="order">
+    <!-- <div class="card result-card" v-for="order in $store.state.createdOrdersTrackingNum" :key="order">
         <h4 class="title is-4">{{order}}</h4>
       </div> -->
 
-      <div class="card result-card" :class="$store.state.createdOrdersTrackingNum.includes(Specimen.accession_number) ? 'is-new' : 'from-db'" v-for="Specimen in Specimens" :key="Specimen.id">
-        <h4 class="title is-4">{{Specimen.accession_number}}</h4>
-      </div>
-    
+    <!-- :class="$store.state.createdOrdersTrackingNum.includes(Specimen.accession_number) ? 'is-new' : 'from-db'" -->
+
+    <div
+      class="card result-card"
+      :class="Specimen.class"
+      v-for="Specimen in Specimens"
+      :key="Specimen.id"
+    >
+      <h4 class="title is-4">{{ Specimen.accession_number }}</h4>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import GetSiteOrders from "@/composables/getSiteOrders"
-import toggleViews from "@/composables/toggleViews"
-import SearchPatient from "@/composables/searchPatient"
+import { defineComponent, watch } from "vue";
+import GetSiteOrders from "@/composables/getSiteOrders";
+import toggleViews from "@/composables/toggleViews";
+import SearchPatient from "@/composables/searchPatient";
 import { Patient } from "@/interfaces/Patient";
 import { MutationTypes, useStore } from "@/store";
 
 export default defineComponent({
   name: "OrderResult",
   setup() {
-
     const store = useStore();
 
-    const { fetchOrders, message, Specimens, code } = GetSiteOrders()
+    const { fetchOrders, Specimens } = GetSiteOrders();
 
-    fetchOrders()
 
-    return { Specimens };
+    fetchOrders();
+
+
+
+    return { Specimens};
   },
 });
 </script>
 
 <style>
+.order-result {
+  padding-top: 15px;
+}
 
-  .order-result {
-    padding-top: 15px;
-  }
+.result-card {
+  width: 422px;
+  height: 150px;
+  border-radius: 8px !important;
+  text-align: center;
+  padding: 15px 5px;
+  cursor: pointer;
+  margin: 10px 15px;
+  display: inline-block;
+}
 
-  .result-card {
-    width: 422px;
-    height: 150px;
-    border-radius: 8px !important;
-    text-align: center;
-    padding: 15px 5px;
-    cursor: pointer;
-    margin:  10px 15px;
-    display: inline-block;
-  }
+.is-new {
+  background-color: #3796ce !important;
+}
 
-  .is-new {
-    background-color: #3796ce !important;
-  }
+.from-db {
+  background-color: #999d9e !important;
+  /* background-color: #2d81b3 !important; */
+}
 
-  .from-db {
-    background-color: #999d9e !important;
-  }
+.results-ready {
+  background-color: #3796ce !important;
+}
 
-  .results-ready {
-    background-color: #3d8612 !important;
-  }
+.result-card:hover {
+  background-color: #2d81b3 !important;
+}
 
-  .result-card:hover {
-    background-color: #2d81b3 !important;
-  }
-
-  .result-card .title {
-    color: white;
-    margin-top: 10px;
-  }
-
+.result-card .title {
+  color: white;
+  margin-top: 10px;
+}
 </style>
