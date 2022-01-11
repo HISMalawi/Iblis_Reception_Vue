7e7547b5dee7fe1b3a494fe7425cd043ee0f2960
Blessings Chidambe <45852972+Cardkess@users.noreply.github.com>
2022-01-11 16:52:45 +0200

    Dashboard Orders Date range UI

--- src/views/DashboardPanel.vue
+++ src/views/DashboardPanel.vue
@@ -1,23 +1,69 @@
 <template>
-  <dashboard-results-panel v-if="panelVisibility" :specimen="SelectedSpecimen" @CloseResultsPanel="ClosePanel"/>
+  <dashboard-results-panel
+    v-if="panelVisibility"
+    :specimen="SelectedSpecimen"
+    @CloseResultsPanel="ClosePanel"
+  />
   <div class="dashboard tile is-10 is-parent">
     <article class="custom-height custom-bg tile is-child">
       <p class="pageTitle subtitle">Dashboard</p>
-    
-        <div class="content has-text-left">
-          <!-- Content -->
-          
-          <order-result @ShowResultsPanel="OpenPanel" />
+      <div class="dashboard-panel-tool-bar">
+
+        <div class="field is-horizontal tool-bar-date-field">
+          <div class="field-label"></div>
+          <div class="field-body">
+            <div class="field">
+              <div class="field has-addons">
+                <p class="control">
+                  <a class="button is-static"> From : </a>
+                </p>
+                <p class="control is-expanded">
+                  <input
+                    class="input"
+                    type="date"
+                    v-model="fromDate"
+                  />
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-      
+
+        <div class="field is-horizontal tool-bar-date-field">
+          <div class="field-label"></div>
+          <div class="field-body">
+            <div class="field">
+              <div class="field has-addons">
+                <p class="control">
+                  <a class="button is-static"> To : </a>
+                </p>
+                <p class="control is-expanded">
+                  <input
+                    class="input"
+                    type="date"
+                    v-model="toDate"
+                  />
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="content has-text-left">
+        <!-- Content -->
+
+        <order-result @ShowResultsPanel="OpenPanel" />
+      </div>
     </article>
   </div>
 </template>
 
 <script lang="ts">
 import { computed, defineComponent, ref } from "vue";
-import OrderResult from "@/views/OrderResult.vue"
-import DashboardResultsPanel from "@/views/DashboardResultsPanel.vue"
+import OrderResult from "@/views/OrderResult.vue";
+import DashboardResultsPanel from "@/views/DashboardResultsPanel.vue";
 import { Specimen } from "@/interfaces/Specimen";
 
 export default defineComponent({
@@ -26,35 +72,59 @@ export default defineComponent({
     OrderResult,
     DashboardResultsPanel,
   },
-  setup() {
+  setup() { 
 
-    const SelectedSpecimen = ref<Specimen | null>()
+    const now = new Date()
+      .toISOString()
+      .replace(/T/, " ") // replace T with a space
+      .replace(/\..+/, "").substring(0,10);
 
-    const panelVisibility = ref<Boolean>(false)
+    const SelectedSpecimen = ref<Specimen | null>();
+
+    const panelVisibility = ref<Boolean>(false);
+
+    const fromDate = ref<string>()
+
+    const toDate = ref<string>()
+
+    fromDate.value = now
+
+    toDate.value = now
 
     const OpenPanel = (Specimen: Specimen) => {
-
-      SelectedSpecimen.value = Specimen
-      panelVisibility.value = true
-
-    }
+      SelectedSpecimen.value = Specimen;
+      panelVisibility.value = true;
+    };
 
     const ClosePanel = () => {
+      panelVisibility.value = false;
+    };
 
-      panelVisibility.value = false
-      
-    }
-
-    return { OpenPanel, ClosePanel, panelVisibility, SelectedSpecimen }
-  }
+    return { OpenPanel, ClosePanel, panelVisibility, SelectedSpecimen, fromDate, toDate };
+  },
 });
 </script>
 
 <style>
-
 .dashboard article {
   padding: 30px;
 }
 
-</style>
+.dashboard-panel-tool-bar {
+  position: relative;
+  top: -55px;
+  display: block !important;
+  text-align: left;
+  margin-left: 35px;
+}
 
+.tool-bar-date-field {
+  display: inline-block !important;
+  width: 300px;
+  padding: 0px !important;
+  margin-bottom: 0px !important;
+  padding-bottom: 0px !important;
+  margin-left: 0px !important;
+  margin-right: 10px;
+}
+</style>
