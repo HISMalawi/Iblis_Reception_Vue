94f9d0b1c7d123dc4cd6cc3666fb18b58e96083a
Hopson Gausi <57468358+hopgausi@users.noreply.github.com>
2022-03-10 10:04:08 +0200

    Printing package dist

---
+++ src/plugins/vue3-print-nb/dist/vue3-print-nb.es.d.ts
@@ -0,0 +1,7 @@
+export default t;
+declare namespace t {
+    export const directiveName: string;
+    export function mounted(t: any, i: any, l: any): void;
+    export function mounted(t: any, i: any, l: any): void;
+    export function install(e: any): void;
+}
---
+++ src/plugins/vue3-print-nb/dist/vue3-print-nb.umd.d.ts
@@ -0,0 +1,4 @@
+export declare const directiveName: string;
+export declare function mounted(t: any, i: any, l: any): void;
+export declare function mounted(t: any, i: any, l: any): void;
+export declare function install(e: any): void;
